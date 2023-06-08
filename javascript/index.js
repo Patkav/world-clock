765719eec201a9311986f3aefd21656b1eeff30d
Patkav <patricia.kavanaugha@gmail.com>
2023-06-07 21:55:20 -0400

    Added the basic HTML

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+<select id="cities">
+  <option value>Select a city</option>
+  <option value="Europe/Paris">Paris</option>
+  <option value="Asia/Tokyo">Tokyo</option>
+  <option value="Australia/Sydney">Sydney</option>
+</select>;
+</select>;
