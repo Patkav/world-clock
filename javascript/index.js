b106d8531de1978a2a7b647d4d43d05e3c7834d3
Patkav <patricia.kavanaugha@gmail.com>
2023-06-11 22:36:40 -0400

    Added JS structure

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,22 @@
-<select id="cities">
-  <option value>Select a city</option>
-  <option value="Europe/Paris">Paris</option>
-  <option value="Asia/Tokyo">Tokyo</option>
-  <option value="Australia/Sydney">Sydney</option>
-</select>;
-</select>;
+function updateLosAngelesTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateLosAngelesTime();
+setInterval(updateLosAngelesTime, 1000);
+//Paris
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+let parisTime = moment().tz("Europe/Paris");
+
+parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
